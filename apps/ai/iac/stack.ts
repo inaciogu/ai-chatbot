11132d785fbf1eb79d2fad709f38e9c7b54f1d6d
Gustavo Inacio <gus.inacio@hotmail.com>
2024-06-23 22:36:52 -0300

    feat: start to build ai app

---
+++ apps/ai/iac/stack.ts
@@ -0,0 +1,15 @@
+import { BaseStack } from '../../commons/iac/base-stack'
+import { Construct } from 'constructs'
+import { StackProps } from 'aws-cdk-lib'
+
+export class AIStack extends BaseStack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, {
+      ...props,
+      serviceName: 'ai',
+    })
+
+    this.createDynamoDb()
+    this.createEventHandlingFunction('apps/ai/ai.handler.ts')
+  }
+}
