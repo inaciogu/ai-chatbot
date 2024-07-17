5f9c9af255643e704a70db66417da2b2b36fd315
Gustavo Inacio <gus.inacio@hotmail.com>
2024-07-17 12:14:40 -0300

    chore: deploy each service individually

--- bin/ai-chatbot-ts.ts
+++ bin/ai-chatbot-ts.ts
@@ -2,33 +2,25 @@
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
 import { resolve, join } from 'path'
-import { existsSync, readdirSync } from 'node:fs'
+import { existsSync } from 'node:fs'
 
 const app = new cdk.App()
 const tenant = app.node.tryGetContext('tenant')
+const service = app.node.tryGetContext('service')
 
 const id = ['chatbot-ai', tenant].join('-')
 const appsDir = resolve(__dirname, '..', 'apps')
 
-const defineStacks = () => {
-  const services = readdirSync(appsDir, { withFileTypes: true })
-
-  services.forEach(async (service) => {
-    if (service.isDirectory()) {
-      const stackPath = join(appsDir, service.name, 'iac', 'stack.ts')
-      try {
-        if (existsSync(stackPath)) {
-          const stack = await import(stackPath)
-          new stack.default(app, `${id}-${service.name}`, {})
-        }
-      } catch (error) {
-        console.error(
-          `Erro ao carregar a stack do serviço ${service.name}:`,
-          error,
-        )
-      }
+const defineStacks = async () => {
+  const stackPath = join(appsDir, service, 'iac', 'stack.ts')
+  try {
+    if (existsSync(stackPath)) {
+      const stack = await import(stackPath)
+      new stack.default(app, `${id}-${service}`, {})
     }
-  })
+  } catch (error) {
+    console.error(`Erro ao carregar a stack do serviço ${service.name}:`, error)
+  }
 }
 
 defineStacks()
